1eb6996e4f687c0ffd837db357c197b06f1087d6
Javier <richards.javier@gmail.com>
2017-07-20 21:10:09 -0400

    cree modelo evaluacion pero no he añadido la relacion

---
+++ resources/views/evaluacion.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+
+     <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Mis Evaluaciones</div>
+                {!!$carpeta!!}
+
+                <div class="panel-body">
+                
+                   <li><a href="#"> Buena</a><input type="file" name="Syllabus" placeholder="Syllabus"></li>
+                   <li><a href="#"> Mala</a><input type="file" name="Lista Clases" placeholder="Lista de Clases"></li>
+                   <li><a href="#"> Normal</a><input type="file" name="Acta" placeholder="Acta"></li>
+                  
+         
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
